41ffafe58ebafc75640fcd8c20b8e64975ed109f
Guillaume DEPARDON <guillaume.depardon@student.42.fr>
2015-03-29 01:23:32 +0100

    [DyLib] Changed the changement logic Now unloading everything because of possible OpenGL context clashes between libraries

--- game/SharedObject.hpp
+++ game/SharedObject.hpp
@@ -3,38 +3,50 @@
 #include <dlfcn.h>
 #include <iostream>
 #include <memory>
+#include <functional>
 
-template <class Interface, class GetterFn, const char * GetterFnName>
+template <class Interface, class GetterFn, const char * getterFnName>
 class SharedObject {
 
 public:
     template <class... GetterArgs>
-    SharedObject(const std::string & objectName, const GetterArgs &... args) {
+    SharedObject(const std::string & objectName, const GetterArgs &... args):
+        handle_ { nullptr } {
+
         handle_ = dlopen(objectName.c_str(), RTLD_NOW | RTLD_LOCAL);
         if (!handle_)
             std::cerr << dlerror() << std::endl;
 
         char *error;
-        auto getterFn = reinterpret_cast<GetterFn>(dlsym(handle_, GetterFnName));
+        auto getterFn = reinterpret_cast<GetterFn>(dlsym(handle_, getterFnName));
         if ((error = dlerror()) != nullptr)
             std::cerr << error << std::endl;
 
-        interface_.reset(getterFn(args...));
+        initF_ = std::bind(getterFn, args...);
     }
 
     ~SharedObject() {
         if (handle_) {
-            interface_.reset();
+            release();
             dlclose(handle_);
         }
     }
 
-    Interface * get() const {
+    Interface * get() {
+        if (!interface_)
+            interface_.reset(initF_());
         return interface_.get();
     }
 
+    void release() {
+        interface_.reset();
+    }
+
 private:
+    using InitF = std::function<Interface * ()>;
+
     void                        *handle_;
+    InitF                       initF_;
     std::unique_ptr<Interface>  interface_;
 
 };
