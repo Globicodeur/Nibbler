1f3209e296d89e44ee3c747a3b8a3fbd17216ede
Guillaume DEPARDON <guillaume.depardon@student.42.fr>
2015-04-10 17:02:03 +0200

    [Qt audio] Added a death sound for Qt + some refactoring

--- audio/qt/Player.cpp
+++ audio/qt/Player.cpp
@@ -1,13 +1,27 @@
 #include "Player.hpp"
 
-QtPlayer::QtPlayer(void):
-    eatSound_ { QUrl::fromLocalFile(
-        QFileInfo("audio/qt/assets/ghost_death.mp3").absoluteFilePath()
-    ) }
-{ }
+#include <QMediaContent>
+#include <QFileInfo>
+
+#include <unordered_map>
+
+static const char EAT_PATH[] = "audio/qt/assets/ghost_death.mp3";
+static const char DIE_PATH[] = "audio/qt/assets/never_say_die.mp3";
+
+static QMediaContent buildContent(const char *filePath) {
+    return QUrl::fromLocalFile(QFileInfo(filePath).absoluteFilePath());
+}
+
+using SoundMap = std::unordered_map<audio::SoundType, QMediaContent>;
+static const SoundMap SOUNDS = {
+    { audio::FoodEaten, buildContent(EAT_PATH) },
+    { audio::Dead,      buildContent(DIE_PATH) },
+};
 
 void QtPlayer::play(audio::SoundType sound) {
-    if (sound == audio::FoodEaten)
-        player.setMedia(eatSound_);
-    player.play();
+    auto soundIt = SOUNDS.find(sound);
+    if (soundIt != SOUNDS.end()) {
+        player.setMedia(soundIt->second);
+        player.play();
+    }
 }
--- audio/qt/Player.hpp
+++ audio/qt/Player.hpp
@@ -1,17 +1,15 @@
 #pragma once
 
 #include <QMediaPlayer>
-#include <QMediaContent>
-#include <QFileInfo>
 
 #include "spec.hpp"
 
 class QtPlayer: public audio::Player {
 
 public:
-                    QtPlayer(void);
 
     // 42 norme
+                    QtPlayer(void)              = default;
     virtual         ~QtPlayer(void)             = default;
                     QtPlayer(const QtPlayer &)  = delete;
     QtPlayer &      operator=(const QtPlayer &) = delete;
@@ -21,5 +19,4 @@ public:
 
 private:
     QMediaPlayer    player;
-    QMediaContent   eatSound_;
 };
