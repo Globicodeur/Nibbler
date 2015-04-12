8824c14b2ec89f139b36c204872f8d61c01012dd
Guillaume DEPARDON <guillaume.depardon@student.42.fr>
2015-04-12 19:26:46 +0200

    [:lipstick:] Found a better dispatch pattern

--- game/Application.cpp
+++ game/Application.cpp
@@ -27,18 +27,18 @@ static void dispatchInput(gui::Input input) {
     using DispatchMap = std::unordered_map<gui::Input, Emitter>;
 
     static const DispatchMap DISPATCH = {
-        { gui::Input::Up,    [] { Dispatcher::emit<Event::UpP1>();       } },
-        { gui::Input::Down,  [] { Dispatcher::emit<Event::DownP1>();     } },
-        { gui::Input::Left,  [] { Dispatcher::emit<Event::LeftP1>();     } },
-        { gui::Input::Right, [] { Dispatcher::emit<Event::RightP1>();    } },
-        { gui::Input::W,     [] { Dispatcher::emit<Event::UpP2>();       } },
-        { gui::Input::S,     [] { Dispatcher::emit<Event::DownP2>();     } },
-        { gui::Input::A,     [] { Dispatcher::emit<Event::LeftP2>();     } },
-        { gui::Input::D,     [] { Dispatcher::emit<Event::RightP2>();    } },
-        { gui::Input::Exit,  [] { Dispatcher::emit<Event::Exit>();       } },
-        { gui::Input::Key1,  [] { Dispatcher::emit<Event::ChangeGui1>(); } },
-        { gui::Input::Key2,  [] { Dispatcher::emit<Event::ChangeGui2>(); } },
-        { gui::Input::Key3,  [] { Dispatcher::emit<Event::ChangeGui3>(); } },
+        { gui::Input::Up,    Dispatcher::emit<Event::UpP1>       },
+        { gui::Input::Down,  Dispatcher::emit<Event::DownP1>     },
+        { gui::Input::Left,  Dispatcher::emit<Event::LeftP1>     },
+        { gui::Input::Right, Dispatcher::emit<Event::RightP1>    },
+        { gui::Input::W,     Dispatcher::emit<Event::UpP2>       },
+        { gui::Input::S,     Dispatcher::emit<Event::DownP2>     },
+        { gui::Input::A,     Dispatcher::emit<Event::LeftP2>     },
+        { gui::Input::D,     Dispatcher::emit<Event::RightP2>    },
+        { gui::Input::Exit,  Dispatcher::emit<Event::Exit>       },
+        { gui::Input::Key1,  Dispatcher::emit<Event::ChangeGui1> },
+        { gui::Input::Key2,  Dispatcher::emit<Event::ChangeGui2> },
+        { gui::Input::Key3,  Dispatcher::emit<Event::ChangeGui3> },
     };
 
     auto actionIt = DISPATCH.find(input);
